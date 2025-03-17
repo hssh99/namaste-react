90b2c58a3a3297c6e0627f7be6658e4c1143c9ba
hssh99 <145898285+hssh99@users.noreply.github.com>
2025-03-17 19:58:02 +0530

    ep-03 commit

--- App.js
+++ App.js
@@ -12,9 +12,38 @@ console.log(heading);
 
 // JSX => React.createElement => ReactElement - JS Object => HTMLElement
 
+
+// React Component
+// Class Based Component - OLD
+// Functional Component - NEW
+
+// Functional Component
+// A normal js function which returns jsx.
+
+const HeadingComponent = () => {
+    return <h1 className="heading">Namaste React Functional Component</h1>
+}
+
+
 const jsxHeading = <h1 id="heading">Namaste React using JSX 🚀</h1>
 console.log(jsxHeading)
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(<HeadingComponent />);
+
+
+/**
+ * Header
+ *  - Logo
+ *  - Nav Items
+ * Body
+ *  - Search
+ *  - RestaurantContainer
+ *      - RestaurantCard
+ * Footer
+ *  - Copyright
+ *  - Links
+ *  - Address
+ *  - Contact
+ */
