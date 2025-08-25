f160465edcbe42153a838482fbc005943ac433e0
Harshal Shirke <145898285+hssh99@users.noreply.github.com>
2025-08-25 23:33:41 +0530

    cleanup func in unmounting

--- src/components/User.js
+++ src/components/User.js
@@ -1,8 +1,24 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const User = ({ name }) => {
   const [count] = useState(0);
   const [count2] = useState(2);
+
+  useEffect(() => {
+    // API Call
+    const timer = setInterval(() => {
+      console.log("Namaste React OP");
+    }, 1000);
+    console.log("useEffect");
+
+    return () => {
+      clearInterval(timer);
+      console.log("useEffect Returned");
+    };
+  }, []);
+
+  console.log("render");
+
   return (
     <div className="user-card">
       <h1>Count: {count}</h1>
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -14,22 +14,33 @@ class UserClass extends React.Component {
     console.log("Constructor called");
   }
 
-  async componentDidMount() {
+  componentDidMount() {
     console.log("Child Component Did Mount");
-    const data = await fetch("https://api.github.com/users/akshaymarch7");
-    const json = await data.json();
+    // const data = await fetch("https://api.github.com/users/akshaymarch7");
+    // const json = await data.json();
 
-    this.setState({
-      userInfo: json,
-    });
-    console.log(json);
+    // this.setState({
+    //   userInfo: json,
+    // });
+    // console.log(json);
+
+    this.timer = setInterval(() => {
+      console.log("Namaste React op");
+    }, 1000);
   }
 
-  componentDidUpdate() {
-    console.log("Component did update");
+  // componentDidUpdate() {
+  //   console.log("Component did update");
+  // }
+  /** To render the component after every state var update */
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.count !== prevState.count) {
+      // code
+    }
   }
 
   componentWillUnmount() {
+    clearInterval(this.timer);
     console.log("Component will unmount");
   }
 
